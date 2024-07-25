19a62c4f0f5e28083eb71e6c68e8f0479b9dc344
byrchard <batuhanrecep06@hotmail.com>
2024-07-25 19:05:30 +0300

    Project connected to localdb/mssql

---
+++ Eastwind.DataAccess/Concrete/EastwindContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eastwind.Entities.Concrete;
+
+namespace Eastwind.DataAccess.Concrete
+{
+    public class EastwindContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Eastwind.DataAccess/Concrete/ProductDal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eastwind.Entities.Concrete;
+
+namespace Eastwind.DataAccess.Concrete
+{
+    public class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (EastwindContext context=new EastwindContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public Product Get(int id)
+        {
+            using (EastwindContext context = new EastwindContext())
+            {
+                return context.Products.SingleOrDefault(p=>p.ProductId==id);
+            }
+        }
+
+        public void Add(Product product)
+        {
+            using (EastwindContext context = new EastwindContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+
+            }
+        }
+        public void Update(Product product)
+        {
+            using (EastwindContext context = new EastwindContext())
+            {
+                
+                context.SaveChanges();
+
+            }
+        }
+        public void Delete(Product product)
+        {
+            using (EastwindContext context = new EastwindContext())
+            {
+
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
