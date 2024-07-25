64eb78a0b7a53fb8f69f09b763b4be029956492f
byrchard <batuhanrecep06@hotmail.com>
2024-07-25 20:33:24 +0300

    For Category object, new interfaces implemented with others

--- Eastwind.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ Eastwind.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +7,7 @@ using Eastwind.Entities.Concrete;
 
 namespace Eastwind.DataAccess.Concrete.EntityFramework
 {
-    public class EastwindContext:DbContext
+    public class EfCategoryDal:EfEntityRepositoryBase<Category,EastwindContext>
     {
-        public DbSet<Product> Products { get; set; }
-
-        public DbSet<Category> Categories { get; set; }
     }
 }
