c48f7ba5eb29c530758bf5bece114de467b1de8b
byrchard <batuhanrecep06@hotmail.com>
2024-07-25 19:06:47 +0300

    Business layer created

---
+++ Eastwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eastwind.DataAccess.Concrete;
+using Eastwind.Entities.Concrete;
+
+namespace Eastwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+        public List<Product> GetAll()
+        {
+            //Business Code
+
+            return _productDal.GetAll();
+        }
+    }
+}
